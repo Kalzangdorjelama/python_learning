dcad0cc3216cf73c3f4946676659d801ed419eb3
kalzang <kalzanglama77@gmail.com>
2025-06-14 22:02:11 +0545

    loops problems solution

--- 13_loops/10_solution.py
+++ 13_loops/10_solution.py
@@ -5,11 +5,11 @@
 import time
 
 wait_time = 1
-max_retries = 5
+wait_retries = 5
 attempts = 0
 
-while attempts < max_retries:
-    print("Attempt", attempts + 1, "- wait time", wait_time, )
+while attempts < wait_retries:
+    print("attempt",attempts + 1, " wait_time",wait_time)
     time.sleep(wait_time)
     wait_time *= 2
     attempts += 1
